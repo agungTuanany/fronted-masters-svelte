2cf88dc40d58862c3a21f239e948ce202b4c0453
agungTuanany <agung.tuanany@gmail.com>
2021-05-12 23:43:43 +0700

    feature(master): chapter-6 building a game | load celebrity details

--- cameoparison-starter/src/screens/Game.svelte
+++ cameoparison-starter/src/screens/Game.svelte
@@ -1,6 +1,12 @@
 <script>
     export let selection;
-    console.log(selection);
+
+    const load_details = async (celeb) => {
+        const res = await fetch(`https://cameo-explorer.netlify.app/celebs/${celeb.id}.json`);
+        return await res.json();
+    };
+    const promises = selection.map((round) => Promise.all([load_details(round.a), load_details(round.b)]));
+    let i = 0;
 </script>
 
 <header>
@@ -8,7 +14,23 @@
 </header>
 
 <div class="game-container">
-    <p>game will go here</p>
+    {#await promises[i] then [a, b]}
+        <div class="game">
+            <div class="card-container">
+                {a.name}
+            </div>
+
+            <div>
+                <button class="same"> same price </button>
+            </div>
+
+            <div class="card-container">
+                {b.name}
+            </div>
+        </div>
+    {:catch}
+        <p class="error">Oops! Failed to load data</p>
+    {/await}
 </div>
 
 <div class="results">
